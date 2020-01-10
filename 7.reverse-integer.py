a39b8adf1ab354e273b382bfff208424249632ef
Haoran Chen <hca144@sfu.ca>
2020-01-10 00:03:30 -0800

    Update 7.reverse-integer.py

--- 7.reverse-integer.py
+++ 7.reverse-integer.py
@@ -7,6 +7,16 @@
 # @lc code=start
 class Solution:
     def reverse(self, x: int) -> int:
-        
+        rev = 0
+
+        # [::-1] reverses a string
+        if x < 0:
+            rev = -1 * int(str(-1 * x)[::-1])
+        else:
+            rev = int(str(x)[::-1])
+        if rev < -2 ** 31 or rev >= 2 ** 31:
+            return 0
+        else:
+            return rev
 # @lc code=end
 
